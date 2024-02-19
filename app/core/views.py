e4784d4dd3d61e1f762131278082bae8b0947be6
Samuel Oliveira <patriciosamuel097@gmail.com>
2024-02-19 15:11:41 -0300

    Inser user data into MySQL database

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
-from .models import User
+from .forms import SignupForm, LoginForm
+from .models import Users
+
+
+def insert_user(userData):
+    name = userData['username']
+    email = userData['email']
+    password = userData['password1']
+    user = Users(username=name, email=email, password=password)
+    user.save()
+    
+def check_user(userData):
+    print(userData['username'])
+    print(userData['password'])
+
+
 
 # Create your views here.
 def index(request):
@@ -16,14 +30,32 @@ def signup(request):
         form = SignupForm(request.POST)
         
         if form.is_valid():
-            form.save()
-            print('valid')
+            insert_user(form.cleaned_data)
             return redirect('/login/')
-        
     else:
         form = SignupForm()
     
     return render(request, 'core/signup.html', {
         'form': form,
     })
+    
+    
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        check_user(form.cleaned_data)
+        print('not')
+        
+        if form.is_valid():
+            check_user(form.cleaned_data)
+            return redirect('/')
+    else:
+        form = LoginForm()
+        
+    return render(request, 'core/login.html', {
+        'form': form,
+    })
+        
+
+    
 
